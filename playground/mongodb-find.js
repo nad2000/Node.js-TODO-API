c14a4e3c809683f0a75b7e6e64de716886ec393b
Rad Cirskis <nad2000@gmail.com>
2018-01-18 20:48:16 +1300

    data retrieval...

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -16,13 +16,35 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
   }
   console.log("Connected to MongoDB server");
 
-  db.collection("Todos").insertOne({
-  }, (err, result) => {
-    if (err) {
-      return console.log(`Unable to insert todo ${err}`);
-    }
-    console.log(JSON.stringify(result.ops, undefined, 2));
-    console.log(result.ops[0]._id.getTimestamp());  // object ID embeds timestamp
+  db.collection("Todos").find().toArray().then(docs => {
+    console.log("All TODOs");
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, err => {
+    console.log("Unable to fetch todos", err);
+  });
+
+  db.collection("Todos").find({
+    _id: new ObjectID("5a6048ed66846113ae1f9533")
+  }).toArray().then(docs => {
+    console.log("A specific TODO");
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, err => {
+    console.log("Unable to fetch todos", err);
+  });
+
+  db.collection("Todos").find({completed: false}).toArray().then(docs => {
+    console.log("Not-completed TODOs");
+    console.log(JSON.stringify(docs, (k, v) => {
+      if (k != "_id") return v;
+    }, 2));
+  }, err => {
+    console.log("Unable to fetch todos", err);
+  });
+
+  db.collection("Todos").find().count().then(count => {
+    console.log(`Total count ${count}`);
+  }, err => {
+    console.log("Unable to fetch todos", err);
   });
 
   db.close();
