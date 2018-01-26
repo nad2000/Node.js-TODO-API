b9e88f428243306012d5304f51c5653c35a99652
Rad Cirskis <nad2000@gmail.com>
2018-01-26 19:06:40 +1300

    play with hasing...

---
+++ playground/hashing.js
@@ -0,0 +1,25 @@
+const {SHA256} = require("crypto-js");
+
+var message = "I am user number 3";
+var hash = SHA256(message).toString();
+
+console.log(`Messge: ${message}`);
+console.log(`Hash: ${hash}`);
+
+var data = {
+  id: 4
+};
+var token = {
+  data,
+  hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
+}
+
+token.data.id = 5;
+
+var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
+
+if (resultHash == token.hash) {
+  console.log("Data was not changed");
+} else {
+  console.log("Data was changed. Do not trust!");
+}
