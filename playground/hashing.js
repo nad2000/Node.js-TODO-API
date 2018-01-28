6c1665f1eb3c5d64c7a0fc894abff9d2f1fdd3de
Rad Cirskis <nad2000@gmail.com>
2018-01-28 21:21:03 +1300

    bcrypt...

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require("crypto-js");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 var message = "I am user number 3";
 var hash = SHA256(message).toString();
@@ -35,3 +36,20 @@ console.log(token.toString());
 var decoded = jwt.verify(token, "123abc");
 console.log("DECODED:", decoded);
 
+
+var password = "p455w0rd!!!"
+
+//
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(`SALT: ${salt}, HASH: ${hash}`);
+  });
+});
+
+var hashedPassword = "$2a$10$g0doEMS/EvhB4JAqRAFSUuzX9WLI1VdjhVRgBZObW6nqOfO/qLuUW";
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+bcrypt.compare("abc123", hashedPassword, (err, res) => {
+  console.log(res);
+});
